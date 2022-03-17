f9090f608aa9450332005f0629653e239b8cc49f
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2022-03-18 00:18:54 +0300

    litle view fix

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="root">
     <div class="Header">
-      <carouselContainer class="carouselContainer"></carouselContainer>
-      <previewContainer class="previewContainer"></previewContainer>
+      <div class="previewContainer">
+        <previewContainer class=""></previewContainer>
+      </div>
+      <div class="carouselContainer">
+        <carouselContainer class=""></carouselContainer>
+      </div>
+      <div class="list">
+        <ul>
+          <li><h1>Новости</h1></li>
+          <li><h1>События</h1></li>
+          <li><h1>Сиськи</h1></li>
+          <li><h1>Письки</h1></li>
+        </ul>
+      </div>
     </div>
     <newsContainer></newsContainer>
   </div>
@@ -25,14 +37,29 @@ export default {
 
 <style>
 .Header{
+  display: table;
   position: relative;
+  height: 110%;
+}
+.list{
+  position: absolute;
+  display: flex;
+  right: 4%;
+  height: 100%;
+}
+ul{
+  margin: auto;
 }
 .previewContainer{
+  display: table-cell;
   position:absolute;
-  top: 0%;
+  z-index: 1;
+  text-align: center;
+  width: 20%;
+  height: 100%;
 }
 .carouselContainer{
+  display: table-cell;
   position: relative;
-  top: 0%;
 }
 </style>
--- src/components/PreviewContainer.vue
+++ src/components/PreviewContainer.vue
@@ -1,17 +1,29 @@
 <template>
     <div class="preview">
-        <img src="../assets/logo.png">
-        <h1>GameBase</h1>
-        <h2>Главный портал о видеоиграх</h2>
+        <div class="inner">
+            <img src="../assets/logo.png">
+            <h1>GameBase</h1>
+            <h2>Главный портал</h2>
+            <h2>о видеоиграх</h2>
+        </div>
     </div>
 </template>
 
 <style>
 .preview{
-    
+    width: 100%;
+    height: 100%;
+    vertical-align: middle;
+    display: flex;
+}
+.inner{
+    margin: auto;
 }
 img{
-    width: 50%;
-    height: auto;
+    width: 60%;
+    height: 60%;
+    text-align: center;
+    vertical-align: middle;
+
 }
 </style>
