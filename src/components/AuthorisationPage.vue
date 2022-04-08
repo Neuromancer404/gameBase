0378d1fcf9a04f29ade39caaf61f65ad3caf11e3
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2022-04-09 00:20:16 +0300

    djkfh

--- src/components/AuthorisationPage.vue
+++ src/components/AuthorisationPage.vue
@@ -59,22 +59,18 @@ export default{
     },
     xhr(){
       var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = catchJSON();
-
+      xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        console.log(myObj);
+        }
+    };
       xmlhttp.open("POST", "registration.php", true);
       xmlhttp.send(); 
   }
 },
 }
 
-function catchJSON(){
-
-  if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        console.log(myObj);
-}
-
-}
 </script>
 
 <style>
