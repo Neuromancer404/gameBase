e899ee38a24964e4c95866770587c9f088771fdc
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2022-04-10 19:23:11 +0300

    auth logic

--- src/components/AuthorisationPage.vue
+++ src/components/AuthorisationPage.vue
@@ -15,16 +15,27 @@
           </button>
         </slot>
       </header>
-
         <section class="modal-body">
           <div class="login">
             <p>Логин:</p>
           </div>
           <div class="password">
             <input class="login" name="login" id="login">
+            <p class="errorOut" v-if="loginVisible">Введите логин</p>
+          </div>
+          <div class="email" v-if="way == 'registration'">
+            <p>E-mail:</p>
+            <input class="email" name="email" id="email">
+            <p class="errorOut" v-if="emailVisible">Введите почту</p>
+          </div>
+          <div class="nickname" v-if="way == 'registration'">
+            <p>Никнейм:</p>
+            <input class="nickname" name="nickname" id="nickname">
+            <p class="errorOut" v-if="nickVisible">Введите никнейм</p>
           </div>
           <p>Пароль:</p>
             <input class="password" name="password" id="password">
+            <p class="errorOut" v-if="passwordVisible">Введите пароль</p>
         </section>
         <footer class="modal-footer">
             <slot name="footer">
@@ -49,36 +60,88 @@ export default{
   },
   data(){
     return{
+      loginVisible: false,
+      passwordVisible: false,
+      nickVisible: false,
+      emailVisible: false
     }
   },
   methods:{
     close() {
       this.$emit('close');
+      this.loginVisible = false
+      this.passwordVisible = false
+      this.nickVisible = false
+      this.emailVisible = false
     },
     clickEvent(){
-      let login = document.getElementById("login").value;
-      let password = document.getElementById("password").value;
+      let login, password, email, nickname 
+      if(this.way == "registration") {
+        email = document.getElementById('email').value;
+        nickname = document.getElementById('nickname').value;
+
+        if(document.getElementById("nickname").value == ''){
+          this.nickVisible = true;
+        }else{this.nickVisible = false}
+        if(document.getElementById("email").value == ''){
+          this.emailVisible = true;
+        }else{this.emailVisible = false}
+      }
+
       let xmlhttp = new XMLHttpRequest();
+      if(document.getElementById("login").value == ''){
+        this.loginVisible = true;
+      }
+      else{
+        this.loginVisible = false;
+      }
+      if(document.getElementById("password").value == ''){
+        this.passwordVisible = true;
+      }
+      else{this.passwordVisible = false;}
+
+
+
+      if(!this.loginVisible && !this.passwordVisible && !this.nickVisible && !this.emailVisible){
+      password = document.getElementById("password").value
+
       xmlhttp.open('POST', './php/'+this.way+'.php', true);
       xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xmlhttp.send("login=" + encodeURIComponent(login) + "&password=" + encodeURIComponent(password));
+      xmlhttp.send("login=" + encodeURIComponent(login) + "&password=" + encodeURIComponent(password) + "&email=" + encodeURIComponent(email) + "&nickname=" + encodeURIComponent(nickname));
       xmlhttp.onreadystatechange = function() {
-         if (this.readyState == 4 && this.status == 200) {
+         if (this.readyState == 4) {
+           if(this.status == 200){
                     console.log(this.responseText);
-                    let obj = JSON.parse(this.responseText);
-                    console.log(obj)
-                 }
-                 else{
-                     console.log("connection error");
-                 }
+                    jsonRunner(this.responseText);
+            }else{
+                    error('connection error');
+                }
+           }
       }
       console.log(this.way);
-    }
+      }
+    },
   },
 }
+function error(data){
+  alert(data);
+}
+function jsonRunner(json){
+    JSON.parse(json, (key, val)=>{
+      if(key == 'errorType'){
+        error(val)
+      }
+      if(key == 'using'){
+        error('Такой логин уже занят :(');
+      }
+    })
+  }
 </script>
 
 <style>
+.errorOut{
+  color: red;
+}
 .modal-fade-enter,
   .modal-fade-leave-active {
     opacity: 0;
